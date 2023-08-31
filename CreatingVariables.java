f80294176964dd2edd60e4caf430aea9a19ba043
ayu sulistina <141293781+ayynxn@users.noreply.github.com>
2023-08-31 13:18:10 +0700

    Add files via upload

---
+++ CreatingVariables.java
@@ -0,0 +1,27 @@
+public class CreatingVariables{
+	public static void main(string[] args){
+	int x, y, age;
+	double seconds, e, checking;
+	string firstname, lastname, title;
+	
+	x=10;
+	y=400;
+	age=39;
+	
+	seconds = 4.17;
+	e = 2.83465392652965;
+	checking = 1.89;
+	
+	firstname = "Graham";
+	lastname = "Mitchell";
+	title = "Mr.";
+	
+	system.out.printIn("The variable x contains " + x);
+	system.out.printIn("The value " + y + " is stored in the variable y.");
+	system.out.printIn("The experiment took " + seconds + " seconds.");
+	system.out.printIn("A favorite irritatinal # is Euler's number: " + e);
+	system.out.printIn("Hopefully you have more than $" + checking + " ! ");
+	system.out.printIn("My name's " + title + " " + firstname + " " + lastname);
+  }  
+}
+	
